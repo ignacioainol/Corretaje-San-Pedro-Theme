b02999c490eb29dca7859f71cda50218a5038ee9
Ignacio <shoik.xtazy@gmail.com>
2020-11-22 19:53:43 -0300

    assets and navbar

---
+++ js/app.js
@@ -0,0 +1,24 @@
+// new Splide('#splide', {
+//     type: 'fade',
+//     rewind: true,
+// }).mount();
+
+$(document).ready(function () {
+
+
+    $(".menu-icon").on("click", function () {
+        $("nav ul").toggleClass("showing");
+    });
+});
+
+// Scrolling Effect
+
+$(window).on("scroll", function () {
+    if ($(window).scrollTop()) {
+        $('nav').addClass('black');
+    }
+
+    else {
+        $('nav').removeClass('black');
+    }
+})
